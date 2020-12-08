430a376540a08bd34bab62d85f93dec26f963e6d
Noyrven <zaharowroman@gmail.com>
2020-12-08 20:02:22 +0100

    update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { Tab, Nav } from 'react-bootstrap'
+import { Tab, Nav, Button } from 'react-bootstrap'
+import Conversations from './Conversations'
+import Contacts from './Contacts'
 
 
 
@@ -9,11 +11,12 @@ const CONTACTS_KEY = 'contacts'
 export default function Sidebar({id}) {
 
     const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+    const conversationOpen = activeKey === CONVERSATIONS_KEY
 
     return (
-        <div style={{ width: '250px' }} className='d-flex dlex-column'>
+        <div style={{ width: '250px' }} className='d-flex flex-column'>
             <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
-                <Nav variant='tabs' className='justify-content-center'>
+                <Nav variant="tabs" className="justify-content-center">
                     <Nav.Item>
                         <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
                     </Nav.Item>
@@ -21,6 +24,20 @@ export default function Sidebar({id}) {
                         <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
                     </Nav.Item>
                 </Nav>
+                <Tab.Content className="border-right overflow-auto flex-grow-1">
+                    <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+                        <Conversations />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey={CONTACTS_KEY}>
+                        <Contacts />
+                    </Tab.Pane>
+                </Tab.Content>
+                <div className='small border-top border-right p-2'>
+                    Your ID: <span className='text-muted'>{id}</span>
+                </div>
+                <Button>
+                    New {conversationOpen ? 'Conversation' : 'Contact'}
+                </Button>
             </Tab.Container>
         </div>
     )
